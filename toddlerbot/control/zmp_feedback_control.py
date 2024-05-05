eda6de6fe07c06dd0472f6cfeaa21f03b8e63391
hshi74 <haochen213300@gmail.com>
2024-05-05 12:54:16 -0700

    Refactor the zmp planner from drake

--- toddlerbot/control/zmp_feedback_control.py
+++ toddlerbot/control/zmp_feedback_control.py
@@ -1,88 +1,69 @@
 import control
 import numpy as np
-from scipy.interpolate import PPoly
+from scipy.interpolate import PPoly, interp1d
 from scipy.linalg import expm
+
+from toddlerbot.utils.constants import GRAVITY
 
 
 class ExponentialPlusPiecewisePolynomial:
-    def __init__(self, K, A, alpha, polynomial_parts):
-        # A: Matrix used in the exponential calculation
-        # alpha: Array of coefficients that interact with the exponential term, per segment
-        # polynomial_parts: List of PPoly objects representing polynomial behavior per segment
+    def __init__(self, K, A, alpha, ppoly):
         self.K = K
         self.A = A
         self.alpha = alpha
-        self.polynomial_parts = polynomial_parts
+        self.ppoly = ppoly
 
     def value(self, t):
-        # Determine which segment we are in based on time t
-        for i, poly in enumerate(self.polynomial_parts):
-            if poly.x[0] <= t <= poly.x[-1]:
-                segment_index = i
-                break
-        # Evaluate the polynomial part
-        poly_value = self.polynomial_parts[segment_index](t)
+        # Evaluate the polynomial part at time t
+        result = self.ppoly(t)
+
+        # Determine the index of the segment that contains the time t
+        segment_index = np.searchsorted(self.ppoly.x, t, side="right") - 1
+        # Ensure the index is within the valid range
+        segment_index = max(0, min(segment_index, len(self.ppoly.x) - 2))
         # Calculate the time offset from the beginning of the segment
-        tj = self.polynomial_parts[segment_index].x[0]
+        tj = self.ppoly.x[segment_index]
         # Compute the exponential part
         exponential = expm(self.A * (t - tj))
-        # Combine the results
-        result = poly_value + self.K @ exponential @ self.alpha[
-            :, segment_index
-        ].reshape(-1, 1)
+        # Compute result combining the polynomial and exponential parts
+        result += (
+            self.K @ exponential @ self.alpha[:, segment_index : segment_index + 1]
+        ).flatten()
 
         return result
 
     def derivative(self, order):
         K_new = self.K
-        for i in range(order):
+        for _ in range(order):
             K_new = K_new @ self.A
 
-        derivative_polynomials = [
-            poly.derivative(order) for poly in self.polynomial_parts
-        ]
         # Derivative for exponential part needs special handling if it involves differentiation w.r.t time
         return ExponentialPlusPiecewisePolynomial(
-            K_new, self.A, self.alpha, derivative_polynomials
+            K_new, self.A, self.alpha, self.ppoly.derivative(order)
         )
 
 
-class ZmpPlanner:
+class ZMPPlanner:
     def __init__(self):
-        self.planned_ = False
-        self.kStationaryThreshold = 1e-6
+        self.planned = False
 
-    def control_com_acc(self, time, x):
-        if not self.planned_:
-            raise ValueError("Plan must be called first.")
-
-        # Eq. 20 in [1]
-        yf = self.zmp_ref_traj[-1]
-        x_bar = x.copy()
-        x_bar[:2] -= yf
-        return self.K_ @ x_bar + self.k2.value(time)
-
-    def plan(self, zmp_ref_traj, height, gravity, Qy, R, plan_t_step):
-        zmp_d_degree = 1
-
-        n_segments = len(zmp_ref_traj) - 1
-        self.zmp_ref_traj = zmp_ref_traj
-        self.Qy_ = Qy
-        self.R_ = R
+    def plan(self, time_steps, zmp_d, x0, com_z, Qy, R):
+        self.time_steps = time_steps
+        self.zmp_d = zmp_d
 
         # Eq. 1 and 2 in [1]
         A = np.zeros((4, 4))
-        A[2:, 2:] = np.eye(2)
+        A[:2, 2:] = np.eye(2)
         B = np.zeros((4, 2))
         B[2:, :] = np.eye(2)
-        C = np.zeros((2, 4))
-        C[:, :2] = np.eye(2)
-        D = -height / gravity * np.eye(2)
+        self.C = np.zeros((2, 4))
+        self.C[:, :2] = np.eye(2)
+        self.D = -com_z / GRAVITY * np.eye(2)
 
         # Eq. 9 - 14 in [1]
-        Q1 = C.T @ Qy @ C
-        R1 = R + D.T @ Qy @ D
-        N = C.T @ Qy @ D
+        Q1 = self.C.T @ Qy @ self.C
+        R1 = R + self.D.T @ Qy @ self.D
+        N = self.C.T @ Qy @ self.D
         R1_inv = np.linalg.inv(R1)
 
         K, S, _ = control.lqr(A, B, Q1, R1, N)
@@ -93,123 +74,151 @@ class ZmpPlanner:
         NB = N.T + B.T @ S
         # Eq. 23, 24 in [1]
         A2 = NB.T @ R1_inv @ B.T - A.T
-        B2 = 2 * (C.T - NB.T @ R1_inv @ D) @ Qy
+        B2 = 2 * (self.C.T - NB.T @ R1_inv @ self.D) @ Qy
         A2_inv = np.linalg.inv(A2)
 
         # Last desired ZMP
-        zmp_ref_last = zmp_ref_traj[-1]
-        tmp4 = np.zeros(4)
+        zmp_ref_last = zmp_d[-1]
+        vec4 = np.zeros(4)
 
+        n_segments = len(zmp_d) - 1
         alpha = np.zeros((4, n_segments))
-        beta = [np.zeros((4, zmp_d_degree + 1)) for _ in range(n_segments)]
-        gamma = [np.zeros((2, zmp_d_degree + 1)) for _ in range(n_segments)]
-        c = [np.zeros((2, zmp_d_degree + 1)) for _ in range(n_segments)]
-
-        beta_poly = [None] * n_segments
-        gamma_poly = [None] * n_segments
-
-        delta_time_vec = np.zeros(zmp_d_degree + 1)
-        delta_time_vec[0] = 1
+        beta = [np.zeros((4, 1)) for _ in range(n_segments)]
+        gamma = [np.zeros((2, 1)) for _ in range(n_segments)]
+        c = [np.zeros((2, 1)) for _ in range(n_segments)]
 
         # Algorithm 1 in [1] to solve for parameters of s2 and k2
         for t in range(n_segments - 1, -1, -1):
-            x1, y1 = zmp_ref_traj[t]
-            x2, y2 = zmp_ref_traj[t + 1]
-
-            c[t][:] = 0
-            c[t][:, : zmp_d_degree + 1] = np.array([[x1, x2 - x1], [y1, y2 - y1]])
-            # switch to zbar coord
-            c[t][:, 0] -= zmp_ref_last
+            # Assume linear interpolation between zmp points
+            c[t][:, 0] = zmp_d[t] - zmp_ref_last
 
             # degree 4
-            beta[t][:, zmp_d_degree] = -A2_inv @ B2 @ c[t][:, zmp_d_degree]
-            gamma[t][:, zmp_d_degree] = (
-                R1_inv @ D @ Qy @ c[t][:, zmp_d_degree]
-                - 0.5 * R1_inv @ B.T @ beta[t][:, zmp_d_degree]
+            beta[t][:, 0] = -A2_inv @ B2 @ c[t][:, 0]
+            gamma[t][:, 0] = (
+                R1_inv @ self.D @ Qy @ c[t][:, 0] - 0.5 * R1_inv @ B.T @ beta[t][:, 0]
             )
 
-            for d in range(zmp_d_degree - 1, -1, -1):
-                beta[t][:, d] = A2_inv @ ((d + 1) * beta[t][:, d + 1] - B2 @ c[t][:, d])
-                gamma[t][:, d] = (
-                    R1_inv @ D @ Qy @ c[t][:, d] - 0.5 * R1_inv @ B.T @ beta[t][:, d]
-                )
+            dt = time_steps[t + 1] - time_steps[t]
+            A2exp = expm(A2 * dt)
 
             if t == n_segments - 1:
-                tmp4 = np.zeros(4)
+                vec4 = -beta[t]
             else:
-                tmp4 = alpha[:, t + 1] + beta[t + 1][:, 0]
+                vec4 = alpha[:, t + 1 : t + 2] + beta[t + 1] - beta[t]
 
-            dt = zmp_ref_traj.duration(t)
-            A2exp = expm(A2 * dt)
-            for i in range(zmp_d_degree + 1):
-                delta_time_vec[i] = dt**i
-            tmp4 = tmp4 - beta[t] @ delta_time_vec
+            alpha[:, t] = (np.linalg.inv(A2exp) @ vec4).squeeze()
 
-            alpha[:, t] = np.linalg.inv(A2exp) @ tmp4
-
-            beta_poly[t] = [PPoly.from_coeffs(beta[t][n, :]) for n in range(4)]
-            gamma_poly[t] = [PPoly.from_coeffs(gamma[t][n, :]) for n in range(2)]
+        # (degree+1, num_vars, num_segments)
+        all_beta_coeffs = np.transpose(np.stack(beta, axis=1), (2, 1, 0))
+        all_gamma_coeffs = np.transpose(np.stack(gamma, axis=1), (2, 1, 0))
 
         # Eq. 25 in [1]
-        beta_traj = PPoly.from_splines(beta_poly, plan_t_step * np.arange(n_segments))
+        beta_traj = PPoly(all_beta_coeffs, time_steps)
         self.s2 = ExponentialPlusPiecewisePolynomial(np.eye(4), A2, alpha, beta_traj)
 
         # Eq. 28 in [1]
-        gamma_traj = PPoly.from_splines(gamma_poly, zmp_ref_traj.get_segment_times())
+        gamma_traj = PPoly(all_gamma_coeffs, time_steps)
         self.k2 = ExponentialPlusPiecewisePolynomial(
             -0.5 * R1_inv @ B.T, A2, alpha, gamma_traj
         )
 
-        # # Computes the nominal CoM trajectory. Also known as the forward pass.
-        # # Eq. 35, 36 in [1]
-        # Az = np.zeros((8, 8))
-        # Az[:4, :4] = A + B @ self.K_
-        # Az[:4, 4:] = -0.5 * B @ self.R1i_ @ B.T
-        # Az[4:, 4:] = A2
-        # Azi = np.linalg.inv(Az)
-        # Bz = np.zeros((8, 2))
-        # Bz[:4, :] = B @ self.R1i_ @ D @ Qy
-        # Bz[4:, :] = B2
+        # for v in [0, 2, 4, 6, 8, 10]:
+        #     print(f"beta: {beta_traj(v)}")
+        #     print(f"gamma: {gamma_traj(v)}")
+        #     print(f"s2: {self.s2.value(v)}")
+        #     print(f"k2: {self.k2.value(v)}")
 
-        # a = np.zeros((8, n_segments))
-        # a[4:, :] = alpha
-        # b_poly = [None] * n_segments
+        # Computes the nominal CoM trajectory. Also known as the forward pass.
+        # Eq. 35, 36 in [1]
+        Az = np.zeros((8, 8))
+        Az[:4, :4] = A + B @ self.K
+        Az[:4, 4:] = -0.5 * B @ R1_inv @ B.T
+        Az[4:, 4:] = A2
+        Azi = np.linalg.inv(Az)
+        Bz = np.zeros((8, 2))
+        Bz[:4, :] = B @ R1_inv @ self.D @ Qy
+        Bz[4:, :] = B2
 
-        # b = [np.zeros((4, zmp_d_degree + 1)) for _ in range(n_segments)]
-        # tmp81 = np.zeros(8)
-        # I48 = np.zeros((4, 8))
-        # I48[:, :4] = np.eye(4)
+        a = np.zeros((8, n_segments))
+        a[4:, :] = alpha
 
-        # x = x0.copy()
-        # x[:2] -= zmp_ref_last
+        b = [np.zeros((4, 1)) for _ in range(n_segments)]
+        I48 = np.zeros((4, 8))
+        I48[:, :4] = np.eye(4)
 
-        # # Algorithm 2 in [1] to solve for the CoM trajectory
-        # for t in range(n_segments):
-        #     dt = plan_t_step
-        #     b[t][:, zmp_d_degree] = -Azi[:4, :] @ Bz @ c[t][:, zmp_d_degree]
-        #     for d in range(zmp_d_degree - 1, -1, -1):
-        #         tmp81[:4] = b[t][:, d + 1]
-        #         tmp81[4:] = beta[t][:, d + 1]
-        #         tmp81 *= d + 1
-        #         b[t][:, d] = Azi[:4, :] @ (tmp81 - Bz @ c[t][:, d])
+        x = x0.copy()
+        x[:2] -= zmp_ref_last
 
-        #     a[:4, t] = x - b[t][:, 0]
+        # Algorithm 2 in [1] to solve for the CoM trajectory
+        for t in range(n_segments):
+            dt = time_steps[t + 1] - time_steps[t]
+            b[t][:, 0] = -Azi[:4, :] @ Bz @ c[t][:, 0]
 
-        #     Az_exp = expm(Az * dt)
-        #     for i in range(zmp_d_degree + 1):
-        #         delta_time_vec[i] = dt**i
-        #     x = I48 @ Az_exp @ a[:, t] + b[t] @ delta_time_vec
+            a[:4, t] = x - b[t][:, 0]
 
-        #     b[t][:2, 0] += zmp_ref_last  # Map CoM position back to world frame
+            Az_exp = expm(Az * dt)
+            x = I48 @ Az_exp @ a[:, t] + b[t].squeeze()
 
-        #     b_poly[t] = [PPoly.from_coeffs(b[t][n, :]) for n in range(2)]
+            b[t][:2, 0] += zmp_ref_last  # Map CoM position back to world frame
 
-        # tmp28 = np.zeros((2, 8))
-        # tmp28[:, :2] = np.eye(2)
-        # b_traj = PPoly.from_splines(b_poly, zmp_ref_traj.get_segment_times())
+        mat28 = np.zeros((2, 8))
+        mat28[:, :2] = np.eye(2)
+        all_b_coeffs = np.transpose(np.stack(b, axis=1), (2, 1, 0))
+        b_traj = PPoly(all_b_coeffs[..., :2], time_steps)
 
-        # Com_ = ExponentialPlusPiecewisePolynomial(tmp28, Az, a, b_traj)
-        # Comd_ = Com_.derivative()
-        # Comdd_ = Comd_.derivative()
+        self.com_pos = ExponentialPlusPiecewisePolynomial(mat28, Az, a, b_traj)
+        self.com_vel = self.com_pos.derivative(1)
+        self.com_acc = self.com_vel.derivative(1)
 
-        self.planned_ = True
+        # for v in [0, 2, 4, 6, 8, 10]:
+        #     print(f"b: {b_traj(v)}")
+        #     print(f"com_pos: {self.com_pos.value(v)}")
+        #     print(f"com_vel: {self.com_vel.value(v)}")
+        #     print(f"com_acc: {self.com_acc.value(v)}")
+
+        self.planned = True
+
+    def compute_optimal_com_acc(self, time, x):
+        if not self.planned:
+            raise ValueError("Plan must be called first.")
+
+        # Eq. 20 in [1]
+        yf = self.zmp_d[-1]
+        x_bar = x.copy()
+        x_bar[:2] -= yf
+        return self.K @ x_bar + self.k2.value(time)
+
+    def com_acc_to_cop(self, x, u):
+        if not self.planned:
+            raise ValueError("Plan must be called first.")
+        return self.C @ x + self.D @ u
+
+    def get_desired_zmp_traj(self):
+        if not self.planned:
+            raise ValueError("Plan must be called first.")
+
+        return self.time_steps, self.zmp_d
+
+    def get_desired_zmp(self, time):
+        if not self.planned:
+            raise ValueError("Plan must be called first.")
+
+        return self.zmp_d[np.searchsorted(self.time_steps, time, side="right") - 1]
+
+    def get_nominal_com(self, time):
+        if not self.planned:
+            raise ValueError("Plan must be called first.")
+
+        return self.com_pos.value(time)
+
+    def get_nominal_com_vel(self, time):
+        if not self.planned:
+            raise ValueError("Plan must be called first.")
+
+        return self.com_vel.value(time)
+
+    def get_nominal_com_acc(self, time):
+        if not self.planned:
+            raise ValueError("Plan must be called first.")
+
+        return self.com_acc.value(time)
