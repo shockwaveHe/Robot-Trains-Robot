Fix the mujoco torso rotation

toddlerbot/policies/__init__.py
@@ -23,15 +23,28 @@ class BasePolicy(ABC):
 
     def warm_up(
         self,
+        warm_up_action: npt.NDArray[np.float32],
         warm_up_duration: float,
     ):
         warm_up_time = np.linspace(
-            0, warm_up_duration, int(warm_up_duration / self.control_dt), endpoint=False
+            0,
+            warm_up_duration,
+            int(warm_up_duration / self.control_dt),
+            endpoint=False,
+            dtype=np.float32,
         )
 
         warm_up_pos = np.zeros(
-            (len(warm_up_time), self.robot.action_dim), dtype=np.float32
+            (len(warm_up_time), warm_up_action.shape[0]), dtype=np.float32
         )
+        for i, t in enumerate(warm_up_time):
+            pos = interpolate(
+                np.zeros_like(warm_up_action),
+                warm_up_action,
+                warm_up_duration,
+                t,
+            )
+            warm_up_pos[i] = pos
 
         return warm_up_time, warm_up_pos
 
@@ -39,10 +52,15 @@ class BasePolicy(ABC):
         self,
         time_curr: float,
         action_curr: npt.NDArray[np.float32],
+        reset_action: npt.NDArray[np.float32],
         reset_duration: float,
     ):
         reset_time = np.linspace(
-            0, reset_duration, int(reset_duration / self.control_dt), endpoint=False
+            0,
+            reset_duration,
+            int(reset_duration / self.control_dt),
+            endpoint=False,
+            dtype=np.float32,
         )
 
         reset_pos = np.zeros((len(reset_time), action_curr.shape[0]), dtype=np.float32)
@@ -50,12 +68,12 @@ class BasePolicy(ABC):
             if t < reset_duration - 0.5:
                 pos = interpolate(
                     action_curr,
-                    np.zeros_like(action_curr),
+                    reset_action,
                     reset_duration - 0.5,
                     t,
                 )
             else:
-                pos = np.zeros_like(action_curr)
+                pos = reset_action
 
             reset_pos[i] = pos
 

toddlerbot/policies/rotate_torso.py
@@ -22,22 +22,27 @@ class RotateTorsoPolicy(BasePolicy):
 
         default_q = np.array(list(robot.init_joint_angles.values()), dtype=np.float32)
 
-        warm_up_duration = 3.0
+        warm_up_duration = 2.0
         sine_duraion = 3.0
         reset_duration = 2.0
         n_sine_signal = 2
         frequency_range = [0.2, 0.5]
         amplitude_min = np.pi / 12
 
+        sho_roll_offset = -np.pi / 12
+        warm_up_action = np.array(
+            list(robot.init_motor_angles.values()), dtype=np.float32
+        )
+        warm_up_action[robot.motor_ordering.index("left_sho_roll")] = sho_roll_offset
+        warm_up_action[robot.motor_ordering.index("right_sho_roll")] = sho_roll_offset
         time_list: List[npt.NDArray[np.float32]] = []
         action_list: List[npt.NDArray[np.float32]] = []
-
-        warm_up_time, warm_up_pos = self.warm_up(warm_up_duration)
-
-        time_list.append(warm_up_time)
-        action_list.append(warm_up_pos)
-
         for joint_name in ["waist_roll", "waist_yaw"]:
+            warm_up_time, warm_up_pos = self.warm_up(warm_up_action, warm_up_duration)
+
+            time_list.append(warm_up_time)
+            action_list.append(warm_up_pos)
+
             joint_idx = robot.joint_ordering.index(joint_name)
 
             mean = (
@@ -45,7 +50,7 @@ class RotateTorsoPolicy(BasePolicy):
             ) / 2
             amplitude_max = robot.joint_limits[joint_name][1] - mean
 
-            for _ in range(n_sine_signal):
+            for i in range(n_sine_signal):
                 sine_signal_config = get_random_sine_signal_config(
                     sine_duraion,
                     self.control_dt,
@@ -60,18 +65,24 @@ class RotateTorsoPolicy(BasePolicy):
                 timed_pos = np.tile(default_q.copy(), (signal.shape[0], 1))
                 timed_pos[:, joint_idx] = signal
                 timed_action = np.zeros_like(timed_pos)
-                for i, pos in enumerate(timed_pos):
+                for j, pos in enumerate(timed_pos):
                     joint_angles = dict(zip(robot.joint_ordering, pos))
                     motor_angles = robot.joint_to_motor_angles(joint_angles)
-                    timed_action[i] = np.array(
+                    sine_action = np.array(
                         list(motor_angles.values()), dtype=np.float32
                     )
+                    timed_action[j] = sine_action + warm_up_action
 
                 time_list.append(time)
                 action_list.append(timed_action)
 
                 reset_time, reset_pos = self.reset(
-                    time[-1], timed_action[-1], reset_duration
+                    time[-1],
+                    timed_action[-1],
+                    warm_up_action
+                    if i < n_sine_signal - 1
+                    else np.zeros_like(timed_action[-1]),
+                    reset_duration,
                 )
 
                 time_list.append(reset_time)