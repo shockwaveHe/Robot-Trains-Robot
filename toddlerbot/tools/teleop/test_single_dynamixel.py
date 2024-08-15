10374d3ca6e62be67df1007d9c76ec870df5c356
Ken Wang <weizhuo2@stanford.edu>
2024-08-14 19:23:43 -0700

    added code to test dynamixel

---
+++ toddlerbot/tools/teleop/test_single_dynamixel.py
@@ -0,0 +1,74 @@
+import time
+import numpy as np
+import serial
+from toddlerbot.actuation.dynamixel.dynamixel_control import DynamixelConfig, DynamixelController
+
+# Create a controller object
+config = DynamixelConfig(
+    port="/dev/tty.usbserial-FT8ISUJY",
+    baudrate=3000000,
+    control_mode=["position"],
+    kP=[2400],
+    kI=[0.0],
+    kD=[2400],
+    kFF2=[0.0],
+    kFF1=[0.0],
+    init_pos=[0.0],
+)
+controller = DynamixelController(config, motor_ids=[1])
+
+# Configure the serial connection
+serial_port = '/dev/tty.usbmodem21301'
+baud_rate = 115200  # Baud rate for the serial communication
+
+# Open the serial port
+try:
+    ser = serial.Serial(serial_port, baud_rate, timeout=1)
+    print(f"Connected to {serial_port} at {baud_rate} baud.")
+except serial.SerialException as e:
+    print(f"Failed to connect to {serial_port}: {e}")
+    exit()
+
+# Read data from the serial port
+try:
+    while True:
+        if ser.in_waiting > 0:
+            data = ser.readline().decode('utf-8').rstrip()
+            print(f"Received: {data}")
+            # convert to float
+            pos = float(data)
+            controller.set_pos(pos=[pos])
+            # time.sleep(0.01)
+except KeyboardInterrupt:
+    print("Stopping the serial reading.")
+
+# Close the serial port
+ser.close()
+print(f"Closed connection to {serial_port}.")
+
+
+
+
+# for i in range(1000):
+#     controller.set_pos(pos=[np.sin(i*0.05)+1.1])
+#     # state_dict = controller.get_motor_state()
+#     print(np.sin(i*0.05)+1.1)
+#     # print(state_dict)
+#     time.sleep(0.001)
+
+# time.sleep(1)
+# controller.set_pos(pos=[0.5])
+# state_dict = controller.get_motor_state()
+# print(state_dict)
+# print('At 0.1')
+
+# time.sleep(1)
+# controller.set_pos(pos=[0.0])
+# state_dict = controller.get_motor_state()
+# print(state_dict)
+# print('At 0.0')
+# time.sleep(1)
+
+# state_dict = controller.get_motor_state()
+# print(state_dict)
+# controller.close_motors()
