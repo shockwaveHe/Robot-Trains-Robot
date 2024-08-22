53bcfdeb8debfc02a2b1141cc65223c5f65ce517
hshi74 <haochen213300@gmail.com>
2024-08-22 16:21:20 -0700

    Update the hip roll joint limit

--- scripts/scp_ckpt.sh
+++ scripts/scp_ckpt.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Variables
+# Assign positional arguments to variables
 TIME_STR="$1"
+ROBOT="${2:-toddlerbot}"  # Use the second argument or a default value
+ENV="${3:-walk}"      # Use the third argument or a default value
+
 REMOTE_USER="hshi74"
 REMOTE_HOST="scdt.stanford.edu"
 REMOTE_REPO_PATH="/move/u/${REMOTE_USER}/projects/toddlerbot"
 LOCAL_REPO_PATH="/home/haochen/projects/toddlerbot"
-RELATIVE_PATH="results/toddlerbot_walk_ppo_${TIME_STR}"
+RELATIVE_PATH="results/${ROBOT}_${ENV}_ppo_${TIME_STR}"
 REMOTE_FOLDER_PATH="${REMOTE_REPO_PATH}/${RELATIVE_PATH}"
 LOCAL_FOLDER_PATH="${LOCAL_REPO_PATH}/${RELATIVE_PATH}"
 
