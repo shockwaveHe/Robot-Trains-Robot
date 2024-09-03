73ab88f9546875e1e5f5da091e2a3ecdeaa5f3f4
hshi74 <haochen213300@gmail.com>
2024-09-02 18:21:43 -0700

    Update policy path

--- toddlerbot/envs/train_mjx.py
+++ toddlerbot/envs/train_mjx.py
@@ -323,7 +323,11 @@ def evaluate(
         env.obs_size, env.privileged_obs_size, env.action_size
     )
     make_policy = ppo_networks.make_inference_fn(ppo_network)  # type: ignore
-    params = model.load_params(os.path.join("results", run_name, "policy"))
+    policy_path = os.path.join("results", run_name, "best_policy")
+    if not os.path.exists(policy_path):
+        policy_path = os.path.join("results", run_name, "policy")
+
+    params = model.load_params(policy_path)
     inference_fn = make_policy(params)
 
     # initialize the state
