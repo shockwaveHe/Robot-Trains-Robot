616d85c1e725aea758d3e8307525e95e0f60ab0d
hshi74 <haochen213300@gmail.com>
2024-09-06 11:32:11 -0700

    Update the slurm script

--- scripts/run_slurm.sh
+++ scripts/run_slurm.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
-#SBATCH --job-name="toddlerbot"
-#SBATCH --output=/move/u/hshi74/output/%A.out
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=4G
-#SBATCH --gres=gpu:a5000:1 
+#SBATCH --job-name=toddlerbot
 #SBATCH --account=move
 #SBATCH --partition=move
+#SBATCH --gres=gpu:a5000:1 
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=3-00:00:00
+#SBATCH --output=/move/u/hshi74/output/%A.out
 
 # Default values for the arguments
 ROBOT="toddlerbot"
@@ -26,9 +27,18 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-# Run the Python script with the parsed arguments
-srun bash -c "/move/u/hshi74/.conda/envs/toddlerbot/bin/python toddlerbot/envs/train_mjx.py \
-    --robot $ROBOT \
-    --env $ENV \
-    --eval $EVAL \
-    --restore $RESTORE"
+
+# Construct the command dynamically based on provided arguments
+COMMAND="/move/u/hshi74/.conda/envs/toddlerbot/bin/python toddlerbot/envs/train_mjx.py --robot $ROBOT --env $ENV"
+
+# Only add --eval and --restore if they are non-empty
+if [ -n "$EVAL" ]; then
+    COMMAND="$COMMAND --eval $EVAL"
+fi
+
+if [ -n "$RESTORE" ]; then
+    COMMAND="$COMMAND --restore $RESTORE"
+fi
+
+# Run the Python script with the constructed command
+srun bash -c "$COMMAND"
