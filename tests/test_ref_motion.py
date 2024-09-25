743cd47610361e959b77ec2177af7cb9b8adb895
hshi74 <haochen213300@gmail.com>
2024-09-24 22:13:01 -0700

    Update the command

--- tests/test_ref_motion.py
+++ tests/test_ref_motion.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
         command_list = [
             np.array([0.1, 0, 0], dtype=np.float32),
             np.array([0, -0.1, 0], dtype=np.float32),
-            np.array([0, 0.0, 0.5], dtype=np.float32),
+            np.array([0, 0.0, 0.2], dtype=np.float32),
             np.array([0, 0, 0], dtype=np.float32),
         ]
 
