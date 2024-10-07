81a3072e5e26f9705b82e0d1fb2bb5006e94807c
hshi74 <haochen213300@gmail.com>
2024-10-07 18:24:39 -0400

    Update the apriltag test

--- tests/test_apriltag.py
+++ tests/test_apriltag.py
@@ -42,10 +42,10 @@ class AprilTagDetector:
                 tuple(map(int, corner)) for corner in detection.corners
             ]
 
-            cv2.line(img, ptA, ptB, (0, 255, 0), 2)
-            cv2.line(img, ptB, ptC, (0, 255, 0), 2)
-            cv2.line(img, ptC, ptD, (0, 255, 0), 2)
-            cv2.line(img, ptD, ptA, (0, 255, 0), 2)
+            cv2.line(img, ptA, ptB, (255, 0, 0), 5)
+            cv2.line(img, ptB, ptC, (255, 0, 0), 5)
+            cv2.line(img, ptC, ptD, (255, 0, 0), 5)
+            cv2.line(img, ptD, ptA, (255, 0, 0), 5)
 
             cX, cY = tuple(map(int, detection.center))
             cv2.circle(img, (cX, cY), 5, (0, 0, 255), -1)
@@ -58,7 +58,7 @@ class AprilTagDetector:
                 (ptA[0], ptA[1] - 15),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 1.0,
-                (0, 255, 0),
+                (255, 0, 0),
                 2,
             )
 
@@ -75,6 +75,30 @@ if __name__ == "__main__":
         np.uint8
     )
 
+    # # Desired output size
+    # target_width = 640
+    # target_height = 480
+
+    # # Get the original image size
+    # original_height, original_width = image.shape[:2]
+
+    # # Calculate the scaling factor for resizing
+    # scaling_factor = max(target_width / original_width, target_height / original_height)
+    # new_width = int(original_width * scaling_factor)
+    # new_height = int(original_height * scaling_factor)
+
+    # # Resize the image to make one dimension fit the target size
+    # resized_image = cv2.resize(image, (new_width, new_height)).astype(np.uint8)
+
+    # # Calculate cropping coordinates
+    # start_x = (new_width - target_width) // 2
+    # start_y = (new_height - target_height) // 2
+    # end_x = start_x + target_width
+    # end_y = start_y + target_height
+
+    # # Crop the image to the target dimensions
+    # image = resized_image[start_y:end_y, start_x:end_x]
+
     intrinsics_color_dict = {
         "fx": 850.0,  # Adjust based on the camera and your experience
         "fy": 850.0,  # Similar to fx for square pixels
@@ -88,6 +112,8 @@ if __name__ == "__main__":
     print("Detected AprilTags:", results)
 
     image_vis = april_detector.vis_tag(image, results)
+
+    cv2.imwrite(f"{args.image}_out.jpg", image_vis)
 
     # Display the annotated image
     cv2.imshow("AprilTag", image_vis)
