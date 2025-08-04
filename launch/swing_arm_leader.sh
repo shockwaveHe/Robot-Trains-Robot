ec21712d05b94cf198dea05c37d06521044deada
shockwaveHe <118010095@link.cuhk.edu.cn>
2025-08-04 14:30:36 -0700

    remove reduntant files.

--- launch/swing_arm_leader.sh
+++ launch/swing_arm_leader.sh
@@ -10,4 +10,4 @@ pause() {
 
 echo "Press enter to runing the swing arm leader"
 cd /home/kukabot/Projects/toddlerbot_internal
-python toddlerbot/policies/run_policy.py --policy swing_arm_leader --ip 192.167.110.216 --robot toddlerbot_2xm
+python toddlerbot/policies/run_policy.py --policy swing_arm_leader --ip 192.168.110.216 --robot toddlerbot_2xm
